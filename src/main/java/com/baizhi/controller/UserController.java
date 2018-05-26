88d46d12f6a72166114988cbb6c01e202961e43f
zhl <811105078@qq.com>
2018-05-26 17:22:47 +0800

    第三次提交

--- src/main/java/com/baizhi/controller/UserController.java
+++ src/main/java/com/baizhi/controller/UserController.java
@@ -13,7 +13,7 @@ public class UserController {
     public String selectAll() {
         System.out.println("第一次修改");
         System.out.println("第二次修改");
-        System.out.println("第三次修改");
+        System.out.println("第三次修改   ");
         return "index";
     }
 
